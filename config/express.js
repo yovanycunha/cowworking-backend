c1aed24c90cf36b3f9b28e17aa22c380cd751ed6
Yovany Cunha <yovany33@gmail.com>
2023-08-04 21:06:38 -0300

    unit tests

--- config/express.js
+++ config/express.js
@@ -2,17 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// module.exports = () => {
-//   const app = express();
-
-//   app.set('port', process.env.PORT || 3000);
-
-//   app.use(bodyParser.json());
-//   app.use(cors());
-
-//   return app;
-// };
-
 const app = express();
 
 app.set('port', process.env.PORT || 3000);

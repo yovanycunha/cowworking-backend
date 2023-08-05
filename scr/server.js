659f59589f19a3c37ff5a5eaacbc57d905021fb6
Yovany Cunha <yovany33@gmail.com>
2023-08-04 21:08:31 -0300

    rename server

--- scr/server.js
+++ scr/server.js
@@ -5,8 +5,8 @@ require('../config/router')(app);
 
 db();
 
-const servidor = app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
   console.log(`CowWorking server listening on port ${app.get('port')}`);
 });
 
-module.exports = servidor;
+module.exports = server;
